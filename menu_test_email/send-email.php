8003d9c98f249d5d83fd607416ab02ec47564665
Ranjan <khanalranjan43@gmail.com>
2023-08-22 20:54:08 +0545

    hooks actions filters

---
+++ menu_test_email/send-email.php
@@ -0,0 +1,24 @@
+<?php 
+
+function send_mail_hook($id, $email){
+    if($id != null){
+        $single_post = get_post($id);
+        $to = $email; 
+        $subject = $single_post->post_title;
+        $message = $single_post->post_content;
+        $headers = array('Content-Type: text/html; charset=UTF-8');
+
+        $sent = wp_mail($to, $subject, $message, $headers);
+        // echo $sent;
+        if ($sent) {
+            echo 'Email sent successfully.';
+        } else {
+            echo 'Email sending failed.';
+        }
+    }
+}
+add_action('custom_email_send_action', 'send_mail_hook',10,2);
+
+
+
+?>
